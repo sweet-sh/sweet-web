526a91abca72d136e05fd8d821d2b9c40c0eb6cc
toBeOfUse <mitchjacov@gmail.com>
2019-05-28 17:24:03 -0400

    fixes part 3

--- app/postingToSweet.js
+++ app/postingToSweet.js
@@ -723,23 +723,23 @@ module.exports = function (app) {
                                     notifier.notify('user', 'reply', originalPoster._id, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
                                 }
 
-                                //NOTIFY PEOPLE WHO BOOSTED THE POST AND ALSO PEOPLE WHO COMMENTED ON OR WERE MENTIONED IN THE ORIGINAL (all together so we can try not to do anyone twice)
+                                //NOTIFY PEOPLE WHO BOOSTED THE POST
                                 var boosterIDs = [];
                                 post.boosts.forEach(boostID => {
                                     Post.findById(boostID).then(boost => {
-                                        boosterIDs.push(boost.author);
+                                        boosterIDs.push(boost.author.toString());
                                         //this is true once we've added a boost author into our array for each boost, so it only runs the last time this is called
                                         if (boosterIDs.length == post.boosts.length) {
                                             //remove duplicate ids for people who've boosted it more than once, 
                                             //and make sure we're not notifying the person who left the comment (this will be necessary if they left it on their own boosted post)
                                             //and make sure we're not notifying the post's author (necessary if they boosted their own post) (they'll have gotten a notification above)
-                                            boosterIDs.filter((v, i, a) => a.indexOf(v) === i && v != req.user._id.toString() && v != originalPoster._id.toString());
+                                            boosterIDs = boosterIDs.filter((v, i, a) => a.indexOf(v) === i && v != req.user._id.toString() && v != originalPoster._id.toString());
                                             boosterIDs.forEach(boosterID => {
                                                 User.findById(boosterID).then(booster => {
                                                     //and make sure we're not notifying anyone who was @ed (they'll have gotten a notification above),
                                                     //or anyone who unsubscribed from the post
-                                                    if (!parsedResult.mentions.includes(booster.username) && !post.unsubscribedUsers.includes(boosterID)) {
-                                                        notifier.notify('user', 'boostedPostReply', boosterID, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
+                                                    if (!parsedResult.mentions.includes(booster.username) && !post.unsubscribedUsers.includes(boosterID.toString())) {
+                                                        notifier.notify('user', 'boostedPostReply', booster._id, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
                                                     }
                                                 }).catch(err => {
                                                     console.log("could not find document for booster " + boosterID + ", error:")
@@ -747,30 +747,43 @@ module.exports = function (app) {
                                                 })
                                             })
 
-                                            //now that we have the full list of boosters, we can check the other "subscribers" against it and then notify them
-                                            var workingSubscribers = post.subscribedUsers.filter(u=>!boosterIDs.contains(u));
-                                            if (postPrivacy == "private") {
-                                                workingSubscribers.forEach(subscriberID => {
-                                                    Relationship.findOne({
-                                                        from: originalPoster.email,
-                                                        toUser: subscriberID,
-                                                        value: "trust"
-                                                    }, {
-                                                        _id: 1
-                                                    }).then(theRelationshipExists => {
-                                                        if (theRelationshipExists) {
-                                                            notifySubscriber(subscriberID);
-                                                        }
-                                                    })
-                                                })
-                                            } else {
-                                                workingSubscribers.forEach(subscriberID => {
-                                                    notifySubscriber(subscriberID);
-                                                })
-                                            }
+                                            //if there are boosters, we notify the other "subscribers" here, because here we have the full list of
+                                            //boosters and can check the subscribers against it before notifying them
+                                            var workingSubscribers = post.subscribedUsers.filter(u=>!boosterIDs.includes(u));
+                                            notifySubscribers(workingSubscribers);
                                         }
                                     })
                                 })
+
+                                //NOTIFY THE OTHER SUBSCRIBERS (PEOPLE WHO WERE MENTIONED IN THE ORGINAL POST AND THOSE WHO COMMENTED ON IT)
+
+                                //if there are boosts for this post, this was called a few lines up from here. otherwise, we do it now
+                                if(post.boosts.length===0){
+                                    notifySubscribers(post.subscribedUsers)
+                                }
+
+                                //checks each subscriber for trustedness if this is a private post, notifies all of 'em otherwise
+                                function notifySubscribers(subscriberList){
+                                    if (postPrivacy == "private") {
+                                        subscriberList.forEach(subscriberID => {
+                                            Relationship.findOne({
+                                                from: originalPoster.email,
+                                                toUser: subscriberID,
+                                                value: "trust"
+                                            }, {
+                                                _id: 1
+                                            }).then(theRelationshipExists => {
+                                                if (theRelationshipExists) {
+                                                    notifySubscriber(subscriberID);
+                                                }
+                                            })
+                                        })
+                                    } else {
+                                        subscriberList.forEach(subscriberID => {
+                                            notifySubscriber(subscriberID);
+                                        })
+                                    }
+                                }
 
                                 function notifySubscriber(subscriberID) {
                                     if ((subscriberID != req.user._id.toString()) // Do not notify the comment's author about the comment
@@ -779,11 +792,11 @@ module.exports = function (app) {
                                         &&
                                         (post.unsubscribedUsers.includes(subscriberID) === false) //don't notify unsubscribed users
                                     ) {
-                                        console.log("Notifying subscribed users")
+                                        console.log("Notifying subscribed user");
                                         User.findById(subscriberID).then((subscriber) => {
-                                            if (!parsedResult.mentions.includes(subscriber.username)) { //don't notify people who are going to be notified anyway bc they're mentioned
+                                            if (!parsedResult.mentions.includes(subscriber.username)) { //don't notify people who are going to be notified anyway bc they're mentioned in the new comment
                                                 if (post.mentions.includes(subscriber.username)) {
-                                                    notifier.notify('user', 'mentionedInPostReply', subscriberID, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
+                                                    notifier.notify('user', 'mentioningPostReply', subscriberID, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
                                                 } else {
                                                     notifier.notify('user', 'subscribedReply', subscriberID, req.user._id, post._id, '/' + originalPoster.username + '/' + post.url, 'post')
                                                 }
