e62949ef48be54ff8f7a6ac22ccfc1fd0e01da78
toBeOfUse <mitchjacov@gmail.com>
2019-06-08 23:46:06 -0400

    resolve merge conflicts

--- databasetransforms/DO_NOT_RUNnewTestPosts.js
+++ databasetransforms/DO_NOT_RUNnewTestPosts.js
@@ -38,17 +38,10 @@ async function createPosts() {
 
     var originalPosts = [];
     for (const poster of posters) {
-<<<<<<< HEAD
-        //each poster will have 200 regular posts
-        //distributed randomly over the past 48 hours
-        for (var i = 0; i < 20000; i++) {
-            var postTime = (new Date()).setHours(new Date().getHours() - (Math.random() * 4800));
-=======
         //each poster will have 20 regular posts
         //distributed randomly over the past 48 hours
         for (var i = 0; i < 20; i++) {
             var postTime = (new Date()).setHours(new Date().getHours() - (Math.random() * 48));
->>>>>>> profiling
             var newpost = new Post({
                 type: 'original',
                 authorEmail: poster.email,
@@ -57,14 +50,8 @@ async function createPosts() {
                 privacy: 'public',
                 timestamp: postTime,
                 lastUpdated: postTime,
-<<<<<<< HEAD
-                rawContent: '<p>asdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
-                parsedContent: '<p>asdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
-                numberOfComments: 0
-=======
                 rawContent: '<p>assdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
                 parsedContent: '<p>assdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
->>>>>>> profiling
             })
 			if(Math.random()<0.15){
 				newpost.type = 'community';
@@ -78,18 +65,6 @@ async function createPosts() {
 
     for (const poster of posters) {
         //and 50 random boosts
-<<<<<<< HEAD
-        for (var i = 0; i < 5000; i++) {
-            var currentTime = new Date().getTime();
-            var target = originalPosts[Math.floor(Math.random() * originalPosts.length)];
-            var targetPostDoc = await Post.findById(target);
-            //keep anyone from boosting a post for the second time
-            if (targetPostDoc.type!="community" && !targetPostDoc.boostsV2.some(b => {
-                    return b.booster.equals(poster.id)
-                })) {
-                var postTime = targetPostDoc.timestamp.getTime() + 10;
-                var boostTime = Math.random() * (currentTime - postTime) + postTime;
-=======
         for (var i = 0; i < 5; i++) {
             var boostTime = new Date();
             var target = originalPosts[Math.floor(Math.random() * originalPosts.length)];
@@ -98,7 +73,6 @@ async function createPosts() {
             if (!targetPostDoc.boostsV2.some(b => {
                     return b.booster.equals(poster.id)
                 })) {
->>>>>>> profiling
                 var newpost = new Post({
                     type: 'boost',
                     authorEmail: poster.email,
@@ -109,46 +83,17 @@ async function createPosts() {
                     lastUpdated: boostTime,
                     boostTarget: target
                 })
-<<<<<<< HEAD
-                await newpost.save().then(savedPost=>{
-=======
                 await newpost.save();
->>>>>>> profiling
                 targetPostDoc.boostsV2.push({
                     booster: poster.id,
                     timestamp: boostTime,
                     boost: newpost._id
                 });
-<<<<<<< HEAD
-                targetPostDoc.save();
-				})
-=======
                 await targetPostDoc.save();
->>>>>>> profiling
             }
         }
     }
 
-<<<<<<< HEAD
-    for (var i = 0; i < 20000; i++) {
-        //and then add 200 random comments just for fun
-        var currentTime = new Date().getTime();
-        var commentAuthor = posters[Math.floor(Math.random() * 6)];
-        Post.findById(originalPosts[Math.floor(Math.random() * originalPosts.length)]).then(post => {
-            var postTime = post.timestamp.getTime();
-            var commentTimestamp = new Date(Math.random() * (currentTime - postTime) + postTime);
-            const comment = {
-                authorEmail: commentAuthor.email,
-                author: commentAuthor.id,
-                timestamp: commentTimestamp,
-                rawContent: "<p>adfjadskl;fjlk;adsjf;lksdj;alfkj</p>",
-                parsedContent: "<p>adfjadskl;fjlk;adsjf;lksdj;alfkj</p>",
-            };
-            post.comments.push(comment);
-            post.numberOfComments = post.comments.length;
-            post.lastUpdated = commentTimestamp;
-            post.save();
-=======
     for (var i = 0; i < 20; i++) {
         //and then add 20 random comments just for fun
         var commentTimestamp = new Date();
@@ -164,31 +109,17 @@ async function createPosts() {
             post.comments.push(comment);
             post.lastUpdated = commentTimestamp;
             await post.save();
->>>>>>> profiling
         })
     }
 }
 
 async function createBoostsV2Posts() {
-<<<<<<< HEAD
-    await Post.deleteMany().then((ok) => {
-        console.log('deleted ' + ok.n + ' old post documents');
-    });
-
-    var originalPosts = [];
-    for (const poster of posters) {
-        //each poster will have 20000 regular posts
-        //distribute randomly over the past 48 hours
-        for (var i = 0; i < 20000; i++) {
-            var postTime = (new Date()).setHours(new Date().getHours() - (Math.random() * 4800));
-=======
     var originalPosts = [];
     for (const poster of posters) {
         //each poster will have 20 regular posts
         //distribute randomly over the past 48 hours
         for (var i = 0; i < 20; i++) {
             var postTime = (new Date()).setHours(new Date().getHours() - (Math.random() * 48));
->>>>>>> profiling
             var newpost = new Post({
                 type: 'original',
                 authorEmail: poster.email,
@@ -197,68 +128,16 @@ async function createBoostsV2Posts() {
                 privacy: 'public',
                 timestamp: postTime,
                 lastUpdated: postTime,
-<<<<<<< HEAD
-                rawContent: '<p>afdasfasdadsdfasdfkjl;adsjfkl;adsj' + i + '</p>',
-                parsedContent: '<p>afdasfasdadsdfasdfkjl;adsjfkl;adsj' + i + '</p>',
-                numberOfComments: 0,
-                boostsV2: [{
-                    booster: poster.id,
-                    timestamp: postTime
-                }]
-            })
-			if(Math.random()<0.15){
-				newpost.type = 'community';
-				newpost.community = communities[Math.floor(Math.random()*2)];
-			}
-            await newpost.save().then(insertedPost => {
-                originalPosts.push(insertedPost._id);
-                if(originalPosts.length % 10000==0){
-                    console.log("posts created: "+originalPosts.length)
-                }
-            });
-=======
                 rawContent: '<p>assdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
                 parsedContent: '<p>assdfasdfkjl;adsjfkl;adsj' + originalPosts.length + '</p>',
                 numberOfComments: 0
             })
             await newpost.save();
             originalPosts.push(newpost._id);
->>>>>>> profiling
         }
     }
 
     for (const poster of posters) {
-<<<<<<< HEAD
-        //and 5000 random boosts
-        for (var k = 0; k < 5000; k++) {
-            var currentTime = new Date().getTime();
-            var targetIndex = Math.floor(Math.random() * originalPosts.length);
-            var target = originalPosts[targetIndex];
-            Post.findOne({
-                _id: target
-            }).then(post => {
-                if (post && post.type!="community") {
-                    if (!post.boostsV2.some(b => {
-                            return b.booster.equals(poster.id)
-                        })) {
-                        var postTime = post.timestamp.getTime() + 10;
-                        var boostTime = new Date(Math.random() * (currentTime - postTime) + postTime);
-                        post.boostsV2.push({
-                            booster: poster.id,
-                            timestamp: boostTime
-                        });
-                        post.lastUpdated = boostTime;
-                        post.save();
-                    }
-                } else {
-                    console.log("post not found or is community");
-                    console.log("id: " + target);
-                    console.log("index: " + targetIndex);
-                }
-            }).catch(err => {
-                console.log("boost no. " + i + " not created:");
-                console.log(err);
-=======
         //and 5 random boosts
         for (var i = 0; i < 5; i++) {
             var boostTime = new Date()
@@ -270,33 +149,10 @@ async function createBoostsV2Posts() {
                 });
                 post.lastUpdated = boostTime
                 await post.save();
->>>>>>> profiling
             })
         }
     }
 
-<<<<<<< HEAD
-    for (var j = 0; j < 20000; j++) {
-        //and then add 200 random comments just for fun
-        var currentTime = new Date().getTime();
-        var commentAuthor = posters[Math.floor(Math.random() * 6)];
-        var targetIndex = Math.floor(Math.random() * originalPosts.length);
-        var target = originalPosts[targetIndex];
-        Post.findById(target).then(post => {
-            var postTime = post.timestamp.getTime() + 10;
-            var commentTimestamp = new Date(Math.random() * (currentTime - postTime) + postTime);
-            const comment = {
-                authorEmail: commentAuthor.email,
-                author: commentAuthor.id,
-                timestamp: commentTimestamp,
-                rawContent: "<p>adfjadskl;fjlk;adsjf;lksdj;alfkj" + j + "</p>",
-                parsedContent: "<p>adfjadskl;fjlk;adsjf;lksdj;alfk" + j + "</p>",
-            };
-            post.comments.push(comment);
-            post.lastUpdated = commentTimestamp;
-            post.numberOfComments = post.comments.length;
-            post.save();
-=======
     for (var i = 0; i < 20; i++) {
         //and then add 20 random comments just for fun
         var commentTimestamp = new Date();
@@ -313,15 +169,10 @@ async function createBoostsV2Posts() {
             post.lastUpdated = commentTimestamp;
             post.numberOfComments++;
             await post.save();
->>>>>>> profiling
         })
     }
 }
 
-<<<<<<< HEAD
-createBoostsV2Posts()
-=======
 createBoostsV2Posts().then(()=>{
     process.exit();
 })
->>>>>>> profiling
