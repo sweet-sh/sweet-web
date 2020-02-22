0110552f2f475f5f26bd5d63705764b84fc143d8
toBeOfUse <mitchjacov@gmail.com>
2020-02-22 00:55:43 -0500

    get joined communities list working

--- vue/components/CommunitiesPageComponents/communitiesList.vue
+++ vue/components/CommunitiesPageComponents/communitiesList.vue
@@ -1,18 +1,35 @@
 <template>
-    <div>
-        <div :key="comm._id" v-for="comm in list">
-            {{comm}}
+  <div>
+    <div v-for="comm in list" :key="comm._id" class="content-box">
+      <div class="row">
+        <div class="col commentContent">
+          <p class="mb-2" style="font-size:1.3em">
+            <img
+              class="postAuthorImage"
+              :src="'/images/communities/' + (comm.imageEnabled ? comm.image: 'cake.svg')"
+            >
+            <strong><a class="authorLink" :href="'/community/' + comm.slug">
+              {{ comm.name }}
+            </a></strong>
+            &middot;
+            <span class="text-muted">
+              {{ comm.membersCount + "member" + (comm.membersCount === 1 ? "" : "s") }}
+            </span>
+          </p>
+          <p v-html="comm.descriptionParsed" />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        list: {
-            type: Array,
-            required: true
-        }
+  props: {
+    list: {
+      type: Array,
+      required: true
     }
+  }
 }
 </script>
--- vue/components/TheCommunitiesPage.vue
+++ vue/components/TheCommunitiesPage.vue
@@ -1,15 +1,126 @@
 <template>
-    <communitiesList :list="joinedCommunities" />
+  <div id="homeBody" class="row justify-content-center">
+    <div class="col-lg-8">
+      <h3 class="page-header mx-2">
+        Communities
+      </h3>
+      <ul id="communitiesTabs" class="nav nav-tabs" role="tablist">
+        <li class="nav-item">
+          <a
+            id="communitiesListTab"
+            class="nav-link"
+            :class="joinedTabOpen ? 'active' : ''"
+            href
+            role="tab"
+            aria-controls="tabs-home"
+            aria-selected="true"
+            @click.prevent="switchToJoinedTab"
+          >
+            Joined
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            id="newCommunityTab"
+            class="nav-link"
+            :class="newCommTabOpen ? 'active' : ''"
+            href
+            role="tab"
+            aria-controls="newCommunityTab"
+            aria-selected="false"
+            @click.prevent="switchToNewCommTab"
+          >
+            Create new
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            id="directoryTab"
+            class="nav-link"
+            :class="directoryTabOpen ? 'active' : ''"
+            href
+            role="tab"
+            aria-controls="directoryTab"
+            aria-selected="false"
+            @click.prevent="switchToDirectoryTab"
+          >
+            Show all
+          </a>
+        </li>
+      </ul>
+
+      <div id="communitiesTabContent" class="tab-content">
+        <div
+          v-if="joinedTabOpen"
+          id="communitiesList"
+          class="tab-pane fade show active"
+          role="tabpanel"
+          aria-labelledby="communitiesListTab"
+        >
+          <div id="resultsContainer" class="compact-container">
+            <communitiesList :list="joinedCommunities" />
+          </div>
+        </div>
+        <div
+          v-if="newCommTabOpen"
+          id="newCommunity"
+          class="tab-pane fade show active"
+          role="tabpanel"
+          aria-labelledby="newCommunityTab"
+        >
+          NEW COMMUNITY FORM...
+        </div>
+        <div
+          v-if="directoryTabOpen"
+          id="directory"
+          class="tab-pane fade show active"
+          role="tabpanel"
+          aria-labelledby="directoryTab"
+        >
+          <div id="directoryResults" class="compact-container">
+            <communitiesList :list="[]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import communitiesList from './CommunitiesPageComponents/communitiesList.vue'
+import infiniteLoader from './SharedSubComponents/infiniteLoaderMixin'
 export default {
-    data() {
-        return {
-            joinedCommunities: JSON.parse(document.getElementById('joinedCommunities').innerHTML)
-        }
+  components: { communitiesList },
+  mixins: [infiniteLoader],
+  data () {
+    return {
+      joinedCommunities: JSON.parse(
+        document.getElementById('joinedCommunities').innerHTML
+      ),
+      currentTab: 0
+    }
+  },
+  computed: {
+    joinedTabOpen () {
+      return this.currentTab === 0
     },
-    components: {communitiesList}
+    newCommTabOpen () {
+      return this.currentTab === 1
+    },
+    directoryTabOpen () {
+      return this.currentTab === 2
+    }
+  },
+  methods: {
+    switchToJoinedTab () {
+      this.currentTab = 0
+    },
+    switchToNewCommTab () {
+      this.currentTab = 1
+    },
+    switchToDirectoryTab () {
+      this.currentTab = 2
+    }
+  }
 }
 </script>
