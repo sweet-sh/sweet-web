98f6a5cec71263de724be46c38769ebcd5713586
Raphael Kabo <raphaelkabo@gmail.com>
2020-09-15 23:39:56 +0100

    Added Vue post feed across site

--- vue/postEditorEntry.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import PostEditor from './components/PostEditor.vue'
-new Vue({
-  el: '#post-editor',
-  template: '<PostEditor />',
-  components: { PostEditor }
-})
--- vue/postFeedEntry.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import PostFeed from './components/PostFeed.vue'
-new Vue({
-  el: '#post-feed',
-  template: '<PostFeed />',
-  components: { PostFeed }
-})
